381c727dd9786653d7bda69d1526a07b03f8e8e5
uratec <uratec@cc217e60-46d1-40e8-9bcc-15505b7dd153>
2015-12-25 17:17:14 +0000

    update bullet copyright

--- MMDAgent/Library_Bullet_Physics/src/src/BulletMultiThreaded/GpuSoftBodySolvers/CPU/btSoftBodySolver_CPU.cpp
+++ MMDAgent/Library_Bullet_Physics/src/src/BulletMultiThreaded/GpuSoftBodySolvers/CPU/btSoftBodySolver_CPU.cpp
@@ -19,7 +19,7 @@ subject to the following restrictions:
 /*           http://www.mmdagent.jp/                                 */
 /* ----------------------------------------------------------------- */
 /*                                                                   */
-/*  Copyright (c) 2009-2014  Nagoya Institute of Technology          */
+/*  Copyright (c) 2009-2015  Nagoya Institute of Technology          */
 /*                           Department of Computer Science          */
 /*                                                                   */
 /* All rights reserved.                                              */
